a4e5aa38107093293fc2a775a954fd27a515a7fe
Aliaksei Kirkouski <crushby@gmail.com>
2025-02-20 17:10:22 +0300

    Zadarma minor refactoring

--- src/main/resources/web/region/bg/utils/sip/zadarma/zadarma.js
+++ src/main/resources/web/region/bg/utils/sip/zadarma/zadarma.js
@@ -1,11 +1,3 @@
-var loadLib = document.createElement("script");
-loadLib.src = "https://my.zadarma.com/webphoneWebRTCWidget/v9/js/loader-phone-lib.js?sub_v=1";
-document.head.appendChild(loadLib);
-
-var loadFn = document.createElement("script");
-loadFn.src = "https://my.zadarma.com/webphoneWebRTCWidget/v9/js/loader-phone-fn.js?sub_v=1";
-document.head.appendChild(loadFn);
-
 function zadarmaLoadWebRTC (key, sip) {
     zadarmaWidgetFn(
         key,
